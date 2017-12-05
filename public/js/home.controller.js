8cf68a888e53bf6f7bd0e1daba85a67abbb5485a
Cassandra Torske <cassandra.torske@gmail.com>
2017-12-05 15:51:37 -0700

    ng-repeat it

--- public/js/home.controller.js
+++ public/js/home.controller.js
@@ -3,11 +3,12 @@ angular.module("app")
 
 function HomeController($http, $stateParams, $state) {
   const vm = this
-
+  const apiUrl = 'localhost:3000/'
 
   vm.$onInit = function() {
-
-    console.log('homecontroller');
-
+    $http.get('http://localhost:3000/').then(function(response) {
+      vm.comments = response.data
+      console.log(vm.comments);
+    })
   }
 }
