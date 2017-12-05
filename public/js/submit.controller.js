9e18e0b3d85e70b8ff475be08cd6b7bcf2598a39
Cassandra Torske <cassandra.torske@gmail.com>
2017-12-05 16:02:17 -0700

    make a comment

--- public/js/submit.controller.js
+++ public/js/submit.controller.js
@@ -3,11 +3,29 @@ angular.module("app")
 
 function SubmitController($http, $stateParams, $state) {
   const vm = this
-
+  vm.submitComment = submitComment
 
   vm.$onInit = function() {
+  }
 
-    console.log('submitcontroller');
+  function submitComment() {
+    console.log('hellooo');
+    if(!vm.comment.name) {
+      $http.post('http://localhost:3000', {
+        comment: vm.comment.comment
+      })
+      .then(function(response) {
+        $state.go('home')
+      })
+    } else {
+      $http.post('http://localhost:3000', {
+        name: vm.comment.name,
+        comment: vm.comment.comment
+      })
+      .then(function(response) {
+        $state.go('home')
+      })
+    }
 
   }
 }
